9eca82e026e00d54e4268b4c5e2948919d094586
Matt Fetterman <navx2810@gmail.com>
2015-03-13 16:22:16 -0400

    added appropriate body to the email, removed required on address 2, set bilPhone to bilPhon

--- checkout.php
+++ checkout.php
@@ -12,6 +12,56 @@
    <?php if ( isset($_REQUEST["checkout"]) ) { ?>
         <h2 id="thank-you">Thank you for your order!</h2>
         <p>An email with your order details will be sent to <?= $_REQUEST["bilEmai"] ?> </p>
+
+    <?php 
+        $headers = "From: shopping@mattfetterman.com \r\n";
+        $headers .= "MIME-Version: 1.0\r\n";
+        $headers .= "Content-Type: text/html; charset=utf8\r\n";
+
+        $message = "<h1>Thank you for your order!</h1>";
+        $message .= "<h2>Order Details</h2>";
+        $message .= "<hr>";
+
+        if ( $_COOKIE["s5Cost"] != 0)
+            $message .= "<p>Samsung Galaxy S5 - " . $_COOKIE["s5Cost"] . "</p>";
+        if ( $_COOKIE["alphaCost"] != 0)
+            $message .= "<p>Alienware Alpha - " . $_COOKIE["alphaCost"] . "</p>";
+        if ( $_COOKIE["iMacCost"] != 0)
+            $message .= "<p>iMac - " . $_COOKIE["iMacCost"] . "</p>";
+        if ( $_COOKIE["mlpCost"] != 0)
+            $message .= "<p>My Little Pony DVD Box Set - " . $_COOKIE["mlpCost"] . "</p>";
+
+        $message .= "<hr>";
+
+        $message .= "<p>Sub-Total - $". $_COOKIE("subtotal") ."</p>";
+        $message .= "<p>Tax - $". $_COOKIE("tax") ."</p>";
+        $message .= "<p>Total - $". $_COOKIE("total") ."</p>";
+
+        $message .= "<br><br><h2>Billing Details</h2>";
+        $message .= "<p>". $_REQUEST["bilName"] ."</p>";
+        $message .= "<p>". $_REQUEST["bilAdd1"] ."</p>";
+        $message .= "<p>". $_REQUEST["bilAdd2"] ."</p> <br>";
+        $message .= "<p>". $_REQUEST["bilCity"] ."</p>";
+        $message .= "<p>". $_REQUEST["bilStat"] ."</p>";
+        $message .= "<p>". $_REQUEST["bilZip"] ."</p>";
+        $message .= "<p>". $_REQUEST["bilPhon"] ."</p>";
+        $message .= "<p>". $_REQUEST["bilEmai"] ."</p> <br><br>";
+
+        $message .= "<h2>Shipping Details</h2>";
+        $message .= "<p>". $_REQUEST["shiName"] ."</p>";
+        $message .= "<p>". $_REQUEST["shiAdd1"] ."</p>";
+        $message .= "<p>". $_REQUEST["shiAdd2"] ."</p> <br>";
+        $message .= "<p>". $_REQUEST["shiCity"] ."</p>";
+        $message .= "<p>". $_REQUEST["shiStat"] ."</p>";
+        $message .= "<p>". $_REQUEST["shiZip"] ."</p>";
+        $message .= "<p>". $_REQUEST["shiPhon"] ."</p>";
+        $message .= "<p>". $_REQUEST["shiEmai"] ."</p> <br><br>";
+
+
+     ?>
+
+    <?php mail( $_REQUEST["bilEmai"], "Order Status", $message, $headers ); ?>
+
    <?php } else { ?>
    <fieldset>
     <legend>Order Details</legend>
@@ -53,6 +103,11 @@
             $iMac_cost = $_POST["iMacQ"] * 2499.99;
             $mlp_cost = $_POST["mlpQ"] * 0.99;
 
+            setcookie("s5Cost", $s5_cost);
+            setcookie("alphaCost", $alpha_cost);
+            setcookie("iMacCost", $iMac_cost);
+            setcookie("mlpCost", $mlp_cost);
+
             if ($_POST["s5Q"] != 0){
              echo "<li> $" . number_format($s5_cost, 2, ".", ",") . "</li>";
          }
@@ -86,6 +141,10 @@
         $tax = $subtotal_cost * 0.06 - ( 0.01 * ($cent_count - 1) );    // This is for accounting for precision issues
         $total = $subtotal_cost + $tax;
 
+        setcookie("subtotal", $subtotal_cost);
+        setcookie("tax", $tax);
+        setcookie("total", $total);
+
          echo '<ul class="none">';
          echo '<li> $' . number_format( $subtotal_cost , 2, ".", ",") . '</li>';
          echo '<li> $' . number_format(  $tax, 2, ".", ",") . '</li>';
@@ -101,7 +160,7 @@
 ?>
 </fieldset>
 
-<form action="checkout.php" action="POST">
+<form action="checkout.php" action="GET">
     <fieldset class="small">
         <legend>Billing Information</legend>
 
@@ -117,7 +176,7 @@
             <div class="clear"></div>
         <div>
             <label for="bilAdd2">address:</label>
-            <input type="text" name="bilAdd2" required>
+            <input type="text" name="bilAdd2">
         </div>
             <div class="clear"></div>
         <div>
@@ -139,7 +198,7 @@
 
         <div>
            <label for="bilPhon">phone:</label>
-           <input type="text" name="bilPhone" required maxlength="10">
+           <input type="text" name="bilPhon" required maxlength="10">
        </div>  
             <div class="clear"></div>
        <div>
@@ -165,7 +224,7 @@
             <div class="clear"></div>
         <div>
             <label for="shiAdd2">address:</label>
-            <input type="text" name="shiAdd2" required>
+            <input type="text" name="shiAdd2">
         </div>
             <div class="clear"></div>
         <div>
